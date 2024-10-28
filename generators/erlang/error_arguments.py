86d88c93c765503f4b1b607be4a47456ae672a92
Bartosz Walkowicz <wallko1@hotmail.com>
2024-10-28 04:55:44 +0100

    VFS-12364 Generate to_json function

---
+++ generators/erlang/error_arguments.py
@@ -0,0 +1,419 @@
+"""Definitions of available error arguments."""
+
+__author__ = "Bartosz Walkowicz"
+__copyright__ = "Copyright (C) 2024 ACK CYFRONET AGH"
+__license__ = "This software is released under the MIT license cited in LICENSE.txt"
+
+
+from enum import Enum
+from typing import Final, List, NamedTuple, Optional
+
+
+INDENT: Final[str] = 4 * " "
+
+
+class ErrorArgToJsonEncoding(NamedTuple):
+    tokens: List[str]
+    json_var: str
+    print_var: Optional[str]
+
+
+class _JsonEncodingStrategy(Enum):
+    DIRECT = "direct"
+    CUSTOM = "custom"
+
+
+class _PrintEncodingStrategy(Enum):
+    DIRECT = "direct"
+    FROM_JSON = "from_json"
+    CUSTOM = "custom"
+
+
+class ErrorArg(NamedTuple):
+    name: str
+    nullable: bool = False
+    fmt_control_sequence: str = "~w"
+
+    json_encoding_strategy: _JsonEncodingStrategy = _JsonEncodingStrategy.DIRECT
+    print_encoding_strategy: _PrintEncodingStrategy = _PrintEncodingStrategy.DIRECT
+
+    def get_erlang_variable_name(self) -> str:
+        return self.name[0].upper() + self.name[1:]
+
+    def generate_to_json_encoding(
+        self, *, is_printed: bool = False, indent_level: int = 1
+    ) -> ErrorArgToJsonEncoding:
+        if self.nullable:
+            return self._generate_nullable_to_json_encoding(
+                is_printed=is_printed, indent_level=indent_level
+            )
+
+        return self._generate_to_json_encoding(
+            is_printed=is_printed, indent_level=indent_level
+        )
+
+    def _generate_to_json_encoding(
+        self, *, is_printed: bool = False, indent_level: int = 1
+    ) -> ErrorArgToJsonEncoding:
+        tokens = []
+
+        erl_var = self.get_erlang_variable_name()
+        json_var = erl_var
+        print_var = None
+
+        if self.json_encoding_strategy == _JsonEncodingStrategy.CUSTOM:
+            json_var = f"{erl_var}Json"
+            tokens.extend(self._generate_json_encoding(
+                erl_var=erl_var,
+                assign_to=json_var,
+                indent_level=indent_level,
+            ) + [",\n"])
+
+        if is_printed:
+            if self.print_encoding_strategy == _PrintEncodingStrategy.DIRECT:
+                print_var = erl_var
+            elif self.print_encoding_strategy == _PrintEncodingStrategy.FROM_JSON:
+                print_var = json_var
+            elif self.print_encoding_strategy == _PrintEncodingStrategy.CUSTOM:
+                print_var = f"{erl_var}Print"
+                tokens.extend(self._generate_print_encoding(
+                    json_var=json_var,
+                    erl_var=erl_var,
+                    assign_to=print_var,
+                    indent_level=indent_level,
+                ) + [",\n"])
+
+        return ErrorArgToJsonEncoding(
+            tokens=tokens,
+            json_var=json_var,
+            print_var=print_var,
+        )
+
+    def _generate_nullable_to_json_encoding(
+        self, *, is_printed: bool = False, indent_level: int = 1
+    ) -> ErrorArgToJsonEncoding:
+        strategy = (
+            self.json_encoding_strategy,
+            self.print_encoding_strategy if is_printed else None,
+        )
+        encoding_function = self._nullable_encoding_dispatch[strategy]
+        return encoding_function(self, indent_level=indent_level)
+
+    def _generate_nullable_direct_json_encoding(
+        self, *, indent_level: int
+    ) -> ErrorArgToJsonEncoding:
+        erl_var = self.get_erlang_variable_name()
+        json_var = f"{erl_var}Json"
+
+        indent = indent_level * INDENT
+        tokens = [f"{indent}{json_var} = utils:undefined_to_null({erl_var}),\n"]
+
+        return ErrorArgToJsonEncoding(tokens=tokens, json_var=json_var, print_var=None)
+
+    def _generate_nullable_direct_json_and_print_from_json_encoding(
+        self, *, indent_level: int
+    ) -> ErrorArgToJsonEncoding:
+        encoding = self._generate_nullable_direct_json_encoding(indent_level=indent_level)
+        return encoding._replace(print_var=encoding.json_var)
+
+    def _generate_nullable_direct_json_and_custom_print_encoding(
+        self, *, indent_level: int
+    ) -> ErrorArgToJsonEncoding:
+        erl_var = self.get_erlang_variable_name()
+        json_var = f"{erl_var}Json"
+
+        print_var = f"{erl_var}Print"
+        print_case_var = f"{erl_var}PrintTmp"
+        print_encoding_tokens = self._generate_print_encoding(
+            json_var=erl_var,
+            erl_var=erl_var,
+            assign_to=print_case_var,
+            indent_level=indent_level + 2,
+        ) + [",\n"]
+
+        tokens = self._generate_nullable_case_statement(
+            indent_level=indent_level,
+            json_var=json_var,
+            print_var=print_var,
+            erl_var=erl_var,
+            encoding_tokens=print_encoding_tokens,
+            json_case_var=erl_var,
+            print_case_var=print_case_var,
+        )
+        return ErrorArgToJsonEncoding(
+            tokens=tokens, json_var=json_var, print_var=print_var
+        )
+
+    def _generate_nullable_custom_json_encoding(
+        self, *, indent_level: int
+    ) -> ErrorArgToJsonEncoding:
+        erl_var = self.get_erlang_variable_name()
+        json_var = f"{erl_var}Json"
+        json_encoding_tokens = self._generate_json_encoding(
+            erl_var=erl_var,
+            indent_level=indent_level + 2,
+        )+ ["\n"]
+        tokens = self._generate_nullable_case_statement(
+            indent_level=indent_level,
+            json_var=json_var,
+            erl_var=erl_var,
+            encoding_tokens=json_encoding_tokens,
+        )
+        return ErrorArgToJsonEncoding(tokens=tokens, json_var=json_var, print_var=None)
+
+    def _generate_nullable_custom_json_and_print_from_json_encoding(
+        self, *, indent_level: int
+    ) -> ErrorArgToJsonEncoding:
+        encoding = self._generate_nullable_custom_json_encoding(indent_level=indent_level)
+        return encoding._replace(print_var=encoding.json_var)
+
+    def _generate_nullable_custom_json_and_direct_print_encoding(
+        self, *, indent_level: int
+    ) -> ErrorArgToJsonEncoding:
+        erl_var = self.get_erlang_variable_name()
+
+        json_var = f"{erl_var}Json"
+        json_case_var = f"{erl_var}JsonTmp"
+        json_encoding_tokens = self._generate_json_encoding(
+            erl_var=erl_var,
+            assign_to=json_case_var,
+            indent_level=indent_level + 2,
+        ) + [",\n"]
+
+        print_var = f"{erl_var}Print"
+
+        tokens = self._generate_nullable_case_statement(
+            indent_level=indent_level,
+            json_var=json_var,
+            print_var=print_var,
+            erl_var=erl_var,
+            encoding_tokens=json_encoding_tokens,
+            json_case_var=json_case_var,
+            print_case_var=erl_var,
+        )
+        return ErrorArgToJsonEncoding(
+            tokens=tokens, json_var=json_var, print_var=print_var
+        )
+
+    def _generate_nullable_custom_json_and_print_encoding(
+        self, *, indent_level: int
+    ) -> ErrorArgToJsonEncoding:
+        erl_var = self.get_erlang_variable_name()
+
+        json_var = f"{erl_var}Json"
+        json_case_var = f"{erl_var}JsonTmp"
+        json_encoding_tokens = self._generate_json_encoding(
+            erl_var=erl_var,
+            assign_to=json_case_var,
+            indent_level=indent_level + 2,
+        ) + [",\n"]
+
+        print_var = f"{erl_var}Print"
+        print_case_var = f"{erl_var}PrintTmp"
+        print_encoding_tokens = self._generate_print_encoding(
+            erl_var=erl_var,
+            json_var=json_case_var,
+            assign_to=print_case_var,
+            indent_level=indent_level + 2,
+        ) + [",\n"]
+
+        tokens = self._generate_nullable_case_statement(
+            indent_level=indent_level,
+            json_var=json_var,
+            print_var=print_var,
+            erl_var=erl_var,
+            encoding_tokens=json_encoding_tokens + print_encoding_tokens,
+            json_case_var=json_case_var,
+            print_case_var=print_case_var,
+        )
+        return ErrorArgToJsonEncoding(
+            tokens=tokens, json_var=json_var, print_var=print_var
+        )
+
+    def _generate_nullable_case_statement(
+        self,
+        *,
+        erl_var: str,
+        json_var: str,
+        print_var: Optional[str] = None,
+        indent_level: int,
+        encoding_tokens: List[str],
+        json_case_var: Optional[str] = None,
+        print_case_var: Optional[str] = None,
+    ) -> List[str]:
+        indent_0 = indent_level * INDENT
+        indent_1 = (indent_level + 1) * INDENT
+        indent_2 = (indent_level + 2) * INDENT
+
+        tokens = []
+
+        if print_var:
+            tokens.append(f"{indent_0}{{{json_var}, {print_var}}}")
+        else:
+            tokens.append(f"{indent_0}{json_var}")
+
+        tokens.append(f" = case {erl_var} of\n")
+
+        tokens.append(f"{indent_1}undefined ->\n")
+        tokens.append(f"{indent_2}{'{null, null}' if print_var else 'null'};\n")
+        tokens.append(f"{indent_1}_ ->\n")
+        tokens.extend(encoding_tokens)
+
+        if json_case_var and print_case_var:
+            tokens.append(f"{indent_2}{{{json_case_var}, {print_case_var}}}\n")
+
+        tokens.append(f"{indent_0}end,\n")
+
+        return tokens
+
+    _nullable_encoding_dispatch = {
+        (_JsonEncodingStrategy.DIRECT, None): _generate_nullable_direct_json_encoding,
+        (
+            _JsonEncodingStrategy.DIRECT,
+            _PrintEncodingStrategy.DIRECT,
+        ): _generate_nullable_direct_json_and_print_from_json_encoding,
+        (
+            _JsonEncodingStrategy.DIRECT,
+            _PrintEncodingStrategy.FROM_JSON,
+        ): _generate_nullable_direct_json_and_print_from_json_encoding,
+        (
+            _JsonEncodingStrategy.DIRECT,
+            _PrintEncodingStrategy.CUSTOM,
+        ): _generate_nullable_direct_json_and_custom_print_encoding,
+        (_JsonEncodingStrategy.CUSTOM, None): _generate_nullable_custom_json_encoding,
+        (
+            _JsonEncodingStrategy.CUSTOM,
+            _PrintEncodingStrategy.FROM_JSON,
+        ): _generate_nullable_custom_json_and_print_from_json_encoding,
+        (
+            _JsonEncodingStrategy.CUSTOM,
+            _PrintEncodingStrategy.DIRECT,
+        ): _generate_nullable_custom_json_and_direct_print_encoding,
+        (
+            _JsonEncodingStrategy.CUSTOM,
+            _PrintEncodingStrategy.CUSTOM,
+        ): _generate_nullable_custom_json_and_print_encoding,
+    }
+
+    def _generate_json_encoding(
+        self, *, erl_var: str, assign_to: Optional[str] = None, indent_level: int = 1
+    ) -> List[str]:
+        return []
+
+    def _generate_print_encoding(
+        self,
+        *,
+        json_var: str,
+        erl_var: str,
+        assign_to: Optional[str] = None,
+        indent_level: int = 1,
+    ) -> List[str]:
+        return []
+
+
+class BinaryArg(ErrorArg):
+    fmt_control_sequence: str = "~ts"
+
+
+class AtomArg(ErrorArg):
+    fmt_control_sequence: str = "~ts"
+    json_encoding_strategy: _JsonEncodingStrategy = _JsonEncodingStrategy.CUSTOM
+
+    def _generate_json_encoding(
+        self, *, erl_var: str, assign_to: Optional[str] = None, indent_level: int = 1
+    ) -> List[str]:
+        indent = indent_level * INDENT
+        assignment = f"{assign_to} = " if assign_to else ""
+        return [f"{indent}{assignment}atom_to_binary({erl_var}, utf8)"]
+
+
+class IntegerArg(ErrorArg):
+    fmt_control_sequence: str = "~B"
+
+
+class ConsumerArg(ErrorArg):
+    fmt_control_sequence: str = "~ts"
+
+    json_encoding_strategy: _JsonEncodingStrategy = _JsonEncodingStrategy.CUSTOM
+    print_encoding_strategy: _PrintEncodingStrategy = _PrintEncodingStrategy.CUSTOM
+
+    def _generate_json_encoding(
+        self, *, erl_var: str, assign_to: Optional[str] = None, indent_level: int = 1
+    ) -> List[str]:
+        indent = indent_level * INDENT
+        assignment = f"{assign_to} = " if assign_to else ""
+        return [f"{indent}{assignment}aai:subject_to_json({erl_var}, utf8)"]
+
+    def _generate_print_encoding(
+        self, *, json_var: str, erl_var: str, assign_to: Optional[str] = None, indent_level: int = 1
+    ) -> List[str]:
+        indent = indent_level * INDENT
+        assignment = f"{assign_to} = " if assign_to else ""
+        return [f"{indent}{assignment}aai:subject_to_printable({erl_var}, utf8)"]
+
+
+
+
+# class ErrorArg(ErrorArg):
+#     def map_to_details(self):
+#         return f"errors:to_json({self.name})"
+
+#     def map_to_format(self):
+#         return f"errors:to_printable({self.name})"
+
+
+class TokenTypeArg(ErrorArg):
+    def map_to_details(self):
+        return f"token_type:to_json({self.name})"
+
+    def map_to_format(self):
+        return f"token_type:to_printable({self.name})"
+
+
+class CaveatArg(ErrorArg):
+    def map_to_details(self):
+        return f"caveats:to_json({self.name})"
+
+    def map_to_format(self):
+        return f"caveats:unverified_description({self.name})"
+
+
+class ServiceArg(ErrorArg):
+    def map_to_details(self):
+        return f"aai:service_to_json({self.name})"
+
+    def map_to_format(self):
+        return f"aai:service_to_printable({self.name})"
+
+
+class ListArg(ErrorArg):
+    def map_to_details(self):
+        return f'str_utils:join_as_binaries({self.name}, <<", ">>)'
+
+    def map_to_format(self):
+        return self.map_to_details()
+
+
+class JsonArg(ErrorArg):
+    pass
+
+
+# TODO handle all types
+def load_argument(arg_yaml: dict) -> ErrorArg:
+    name = arg_yaml["name"]
+    arg_type = arg_yaml.get("type", "binary")
+    nullable = arg_yaml.get("nullable", False)
+
+    arg_classes = {
+        "binary": BinaryArg,
+        "atom": AtomArg,
+        "integer": IntegerArg,
+        "error": ErrorArg,
+        "consumer": ConsumerArg,
+        "token_type": TokenTypeArg,
+        "caveat": CaveatArg,
+        "service": ServiceArg,
+        "list": ListArg,
+        "json": JsonArg,
+    }
+    return arg_classes.get(arg_type, BinaryArg)(name, nullable)
--- generators/erlang/gen_erl.py
+++ generators/erlang/gen_erl.py
@@ -5,8 +5,12 @@ __copyright__ = "Copyright (C) 2024 ACK CYFRONET AGH"
 __license__ = "This software is released under the MIT license cited in LICENSE.txt"
 
 import os
+import re
 import shutil
+
 from typing import Final, List, NamedTuple
+
+from error_arguments import ErrorArg, load_argument
 
 import yaml
 
@@ -29,6 +33,9 @@ ERRORS_ERL_TEMPLATE: Final[str] = read_template("errors.erl.template")
 OD_ERROR_FILE_PATH: Final[str] = os.path.join(OUTPUT_DIR, "od_error.erl")
 OD_ERROR_TEMPLATE: Final[str] = read_template("od_error.erl.template")
 
+ERROR_TYPES_DIR: Final[str] = os.path.join(OUTPUT_DIR, "types")
+ERROR_TEMPLATE: Final[str] = read_template("error.erl.template")
+
 
 INDENT: Final[str] = 4 * " "
 HORIZONTAL_COMMENT_LINE: Final[str] = "%%" + 68 * "-"
@@ -38,7 +45,9 @@ class OdError(NamedTuple):
     name: str
     type: str
     id: str
-    args: List[str]
+    description: str
+    http_code: int
+    args: List[ErrorArg]
 
     def get_id_macro(self) -> str:
         return f"ERROR_{self.name.upper()}_ID"
@@ -47,7 +56,7 @@ class OdError(NamedTuple):
         return f"ERROR_{self.name.upper()}_TYPE"
 
     def get_args_as_erlang_variable_names(self) -> List[str]:
-        return [arg[0].upper() + arg[1:] for arg in self.args]
+        return [arg.get_erlang_variable_name() for arg in self.args]
 
     def get_error_macro(self) -> str:
         if not self.args:
@@ -66,9 +75,11 @@ def main():
     clean_output_dir()
 
     error_groups = load_error_groups()
+
     generate_errors_hrl(error_groups)
-    generate_od_error()
-    generate_errors_erl(error_groups)
+    generate_od_error_behaviour()
+    generate_errors_interface_module(error_groups)
+    generate_error_modules(error_groups)
 
 
 def clean_output_dir() -> None:
@@ -77,29 +88,41 @@ def clean_output_dir() -> None:
 
 
 def load_error_groups() -> List[OdErrorGroup]:
-    error_groups = []
-
-    for parent_dir_path, _, file_names in os.walk(ERROR_DEFINITIONS_ROOT_DIR):
-        if file_names:
-            file_names.sort()
-            error_groups.append(create_error_group(parent_dir_path, file_names))
-
+    error_groups = [
+        create_error_group(parent_dir_path, sorted(file_names))
+        for parent_dir_path, _, file_names in os.walk(ERROR_DEFINITIONS_ROOT_DIR)
+        if file_names
+    ]
     return sorted(error_groups, key=lambda x: x.name)
 
 
 def create_error_group(parent_dir_path: str, file_names: List[str]) -> OdErrorGroup:
-    od_errors = [load_error_definition(os.path.join(parent_dir_path, file_name))
-                 for file_name in file_names]
+    od_errors = [
+        load_error_definition(os.path.join(parent_dir_path, file_name))
+        for file_name in file_names
+    ]
     return OdErrorGroup(
         name=os.path.relpath(parent_dir_path, ERROR_DEFINITIONS_ROOT_DIR),
-        errors=od_errors
+        errors=od_errors,
     )
 
 
 def load_error_definition(yaml_definition_path: str) -> OdError:
     yaml_data = read_yaml_file(yaml_definition_path)
     error_name = extract_error_name(yaml_definition_path)
-    return create_od_error(error_name, yaml_data)
+    args = []
+
+    for arg in yaml_data.get("args", []):
+        args.append(load_argument(arg))
+
+    return OdError(
+        name=error_name,
+        type=f"od_error_{error_name}",
+        id=yaml_data["id"],
+        description=yaml_data["description"],
+        http_code=yaml_data["http_code"],
+        args=args,
+    )
 
 
 def read_yaml_file(file_path: str) -> dict:
@@ -109,15 +132,6 @@ def read_yaml_file(file_path: str) -> dict:
 
 def extract_error_name(file_path: str) -> str:
     return os.path.splitext(os.path.basename(file_path))[0]
-
-
-def create_od_error(error_name: str, yaml_data: dict) -> OdError:
-    return OdError(
-        name=error_name,
-        type=f"od_error_{error_name}",
-        id=yaml_data["id"],
-        args=yaml_data.get("args", [])
-    )
 
 
 def generate_errors_hrl(error_groups: List[OdErrorGroup]) -> None:
@@ -143,25 +157,27 @@ def generate_errors_hrl_group_header(error_group: OdErrorGroup) -> List[str]:
     return [
         HORIZONTAL_COMMENT_LINE,
         f"%% {error_group.name} errors",
-        HORIZONTAL_COMMENT_LINE
+        HORIZONTAL_COMMENT_LINE,
     ]
 
 
 def generate_errors_hrl_definitions(od_errors: List[OdError]) -> List[str]:
     lines = []
     for od_error in od_errors:
-        lines.extend([
-            build_error_id_macro_definition(od_error),
-            build_error_type_macro_definition(od_error),
-            build_error_macro_definition(od_error),
-            ""
-        ])
+        lines.extend(
+            [
+                build_error_id_macro_definition(od_error),
+                build_error_type_macro_definition(od_error),
+                build_error_macro_definition(od_error),
+                "",
+            ]
+        )
 
     return lines
 
 
 def build_error_id_macro_definition(od_error: OdError) -> str:
-    return f"-define({od_error.get_id_macro()}, <<\"{od_error.id}\">>)."
+    return f'-define({od_error.get_id_macro()}, <<"{od_error.id}">>).'
 
 
 def build_error_type_macro_definition(od_error: OdError) -> str:
@@ -171,36 +187,141 @@ def build_error_type_macro_definition(od_error: OdError) -> str:
 def build_error_macro_definition(od_error: OdError) -> str:
     error_type_macro = f"?{od_error.get_type_macro()}"
     error_macro = od_error.get_error_macro()
-    
+
     if od_error.args:
         args = ", ".join(od_error.get_args_as_erlang_variable_names())
         error_expansion = f"?ERROR({error_type_macro}, {{{args}}})"
     else:
         error_expansion = f"?ERROR({error_type_macro})"
-    
+
     return f"-define({error_macro}, {error_expansion})."
 
 
-def generate_od_error() -> None:
+def generate_od_error_behaviour() -> None:
     with open(OD_ERROR_FILE_PATH, "w") as f:
         f.write(OD_ERROR_TEMPLATE)
 
 
-def generate_errors_erl(error_groups: List[OdErrorGroup]) -> None:
-    types = [generate_error_dialyzer_type(od_error)
-             for group in error_groups
-             for od_error in group.errors]
+def generate_errors_interface_module(error_groups: List[OdErrorGroup]) -> None:
+    types = [
+        generate_error_dialyzer_type(od_error)
+        for group in error_groups
+        for od_error in group.errors
+    ]
 
-    id_to_type_mapping = [generate_error_id_to_type_mapping(od_error)
-                          for group in error_groups
-                          for od_error in group.errors]
+    id_to_type_mapping = [
+        generate_error_id_to_type_mapping(od_error)
+        for group in error_groups
+        for od_error in group.errors
+    ]
 
     erl_content = ERRORS_ERL_TEMPLATE.format(
-        types=" |\n".join(types),
-        id_to_type_mapping=",\n".join(id_to_type_mapping)
+        types=" |\n".join(types), id_to_type_mapping=",\n".join(id_to_type_mapping)
     )
 
     write_to_file(ERRORS_ERL_FILE_PATH, erl_content)
+
+
+def generate_error_modules(error_groups: List[OdErrorGroup]) -> None:
+    for group in error_groups:
+        group_dir = os.path.join(ERROR_TYPES_DIR, group.name)
+        os.makedirs(group_dir, exist_ok=True)
+
+        for od_error in group.errors:
+            generate_error_module(od_error, group_dir)
+
+
+def generate_error_module(od_error: OdError, output_dir: str) -> None:
+    to_http_code = f"to_http_code(_) -> {od_error.http_code}."
+
+    erl_content = ERROR_TEMPLATE.format(
+        error_type=od_error.type,
+        to_json=generate_to_json_callback(od_error),
+        from_json=generate_from_json_callback(od_error),
+        to_http_code=to_http_code,
+    )
+
+    file_path = os.path.join(output_dir, f"{od_error.type}.erl")
+    write_to_file(file_path, erl_content)
+
+
+def generate_to_json_callback(od_error: OdError) -> str:
+    encoding_tokens = []
+    details_tokens = []
+    description_tokens = [f'<<"{od_error.description}">>\n']
+
+    if od_error.args:
+        details_tokens.append(f'{2*INDENT}<<"details">> => #{{\n')
+
+        fmt_placeholders = re.findall(r"\{(\w+)\}", od_error.description)
+        fmt_placeholder_to_print_var = {}
+        fmt_placeholder_to_control_sequence = {}
+
+        for arg in od_error.args:
+            arg_encoding = arg.generate_to_json_encoding(
+                is_printed=arg.name in fmt_placeholders
+            )
+
+            if arg_encoding.tokens:
+                encoding_tokens.extend(arg_encoding.tokens)
+                encoding_tokens.append("\n")
+
+            details_tokens.extend(
+                [3 * INDENT, f'<<"{arg.name}">> => {arg_encoding.json_var}', ",\n"]
+            )
+
+            fmt_placeholder_to_print_var[arg.name] = arg_encoding.print_var
+            fmt_placeholder_to_control_sequence[arg.name] = arg.fmt_control_sequence
+
+        # replace last comma with closing bracket
+        details_tokens[-1] = f"\n{2*INDENT}}},\n"
+
+        fmt_str = od_error.description.format(**fmt_placeholder_to_control_sequence)
+        print_vars = [
+            fmt_placeholder_to_print_var[placeholder]
+            for placeholder in fmt_placeholders
+        ]
+        description_tokens = [
+            "?fmt(\n",
+            f'{3*INDENT}"{fmt_str}",\n',
+            f"{3*INDENT}[",
+            ", ".join(print_vars),
+            "]\n",
+            f"{2*INDENT})\n",
+        ]
+
+    tokens = [
+        f"to_json(?{od_error.get_error_macro()}) ->\n",
+        *encoding_tokens,
+        f"{INDENT}#{{\n",
+        f'{2*INDENT}<<"id">> => ?{od_error.get_id_macro()},\n',
+        *details_tokens,
+        f'{2*INDENT}<<"description">> => ',
+        *description_tokens,
+        f"{INDENT}}}.",
+    ]
+
+    return "".join(tokens)
+
+
+def generate_from_json_callback(od_error: OdError) -> str:
+    lines = [
+        f"from_json(#{{'id' := ?{od_error.get_id_macro()}, 'details' := Details}}) ->"
+    ]
+    if od_error.args:
+        for arg in od_error.args:
+            if arg.nullable:
+                lines.append(
+                    f'    {arg.name} = maps:get(<<"{arg.name}">>, Details, undefined),'
+                )
+            else:
+                lines.append(f'    {arg.name} = maps:get(<<"{arg.name}">>, Details),')
+        lines.append(
+            f"    ?{od_error.get_error_macro()}({', '.join(arg.name for arg in od_error.args)});"
+        )
+    else:
+        lines.append(f"    ?{od_error.get_error_macro()};")
+    return "\n".join(lines)
 
 
 def generate_error_dialyzer_type(od_error: OdError) -> str:
